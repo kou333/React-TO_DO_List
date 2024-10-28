35073f44127d6e03d4d9ef13cca24353f9acb366
koustubh <55283409+kou333@users.noreply.github.com>
2024-10-28 12:53:02 +0000

    modification

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 // pages/index.js
+// pages/index.js
+import TaskForm from '../components/TaskForm';
+
 export default function Home() {
-    return <h1>Welcome to Your To-Do List App!</h1>;
-  }
-  
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">To-Do List</h1>
+      <TaskForm />
+      {/* Add Task List Display Here */}
+    </div>
+  );
+}
